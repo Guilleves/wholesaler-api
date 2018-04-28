82c070030d88ad088cf9a0d83ccf30e4130397cc
Juan Cruz Grasso <jgrasso@primerosystems.com>
2018-04-28 15:55:27 -0300

    Testing and fixing creation schema script.

--- wholesaler-schema.sql
+++ wholesaler-schema.sql
@@ -1,23 +1,26 @@
-CREATE SCHEMA IF NOT EXISTS wholesaler;
+DROP SCHEMA wholesaler;
+CREATE SCHEMA wholesaler;
+
+USE wholesaler;
 
 DROP TABLE IF EXISTS Organization;
 CREATE TABLE Organization (
     id INT AUTO_INCREMENT,
-    name VARCHAR(),
-    legalName VARCHAR(),
-    cuit VARCHAR(),
-    role VARCHAR(),
+    name VARCHAR(50),
+    legalName VARCHAR(50),
+    cuit VARCHAR(20),
+    role VARCHAR(20),
     PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS User;
 CREATE TABLE User (
     id INT AUTO_INCREMENT,
-    firstName VARCHAR(),
-    lastName VARCHAR(),
-    username VARCHAR(),
-    email VARCHAR(),
-    password VARCHAR(),
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    username VARCHAR(50),
+    email VARCHAR(100),
+    password VARCHAR(256),
     organizationId INT,
     PRIMARY KEY (id),
     CONSTRAINT fk_user_organizaion FOREIGN KEY (organizationId) REFERENCES Organization (id)
@@ -26,22 +29,22 @@ CREATE TABLE User (
 DROP TABLE IF EXISTS Brand;
 CREATE TABLE Brand (
     id INT AUTO_INCREMENT,
-    name VARCHAR(),
+    name VARCHAR(50),
     PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Category;
 CREATE TABLE Category (
     id INT AUTO_INCREMENT,
-    name VARCHAR(),
+    name VARCHAR(50),
     PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS Product;
 CREATE TABLE Product (
     id INT AUTO_INCREMENT,
-    name VARCHAR(),
-    gtin VARCHAR(),
+    name VARCHAR(50),
+    gtin VARCHAR(50),
     brandId INT,
     categoryId INT,
     PRIMARY KEY (id),
@@ -54,6 +57,7 @@ CREATE TABLE Catalog (
     id INT AUTO_INCREMENT,
     organizationId INT,
     productId INT,
+	PRIMARY KEY (id),
     CONSTRAINT fk_catalog_organization FOREIGN KEY (organizationId) REFERENCES Organization (id),
     CONSTRAINT fk_catalog_product FOREIGN KEY (productId) REFERENCES Product (id)
 );
@@ -63,7 +67,7 @@ CREATE TABLE Proposal (
     id INT AUTO_INCREMENT,
     beginDate TIMESTAMP,
     endDate TIMESTAMP,
-    description VARCHAR(),
+    description VARCHAR(100),
     supplierId INT,
     PRIMARY KEY (id),
     CONSTRAINT fk_proposal_organization FOREIGN KEY (supplierId) REFERENCES Organization (id)
@@ -76,12 +80,12 @@ CREATE TABLE ProposalLine (
     proposalId INT,
     productId INT,
     PRIMARY KEY (id),
-    CONSTRAINT fk_proposalLine_product FOREIGN KEY (productId) REFERENCES Product (id)
+    CONSTRAINT fk_proposalLine_product FOREIGN KEY (productId) REFERENCES Product (id),
     CONSTRAINT fk_proposalLine_proposal FOREIGN KEY (proposalId) REFERENCES Proposal (id)
 );
 
-DROP TABLE IF EXISTS Order;
-CREATE TABLE Order (
+DROP TABLE IF EXISTS `Order`;
+CREATE TABLE `Order` (
     id INT AUTO_INCREMENT,
     dateOrdered TIMESTAMP,
     retailId INT,
@@ -96,6 +100,6 @@ CREATE TABLE OrderLine (
     orderId INT,
     proposalLineId INT,
     PRIMARY KEY (id),
-    CONSTRAINT fk_orderLine_order FOREIGN KEY (orderId) REFERENCES Order (id),
-    CONSTRAINT fk_orderLine_proposalLine FOREIGN KEY (proposalLineId) REFERENCES ProposalLine (id)    
+    CONSTRAINT fk_orderLine_order FOREIGN KEY (orderId) REFERENCES `Order` (id),
+    CONSTRAINT fk_orderLine_proposalLine FOREIGN KEY (proposalLineId) REFERENCES ProposalLine (id)
 );
