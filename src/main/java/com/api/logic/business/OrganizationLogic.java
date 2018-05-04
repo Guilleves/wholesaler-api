d342867403ca605ba79e098885f41c3fb02e579b
guillermina <guilleves@gmail.com>
2018-05-04 18:37:59 -0300

    fix indentation

--- src/main/java/com/api/logic/business/OrganizationLogic.java
+++ src/main/java/com/api/logic/business/OrganizationLogic.java
@@ -16,55 +16,55 @@ import com.api.data.business.OrganizationDataAccess;
 // #endregion
 
 public class OrganizationLogic {
-    private OrganizationDataAccess oda;
+  private OrganizationDataAccess oda;
 
-    public OrganizationLogic() {
-        oda = new OrganizationDataAccess();
+  public OrganizationLogic() {
+    oda = new OrganizationDataAccess();
+  }
+
+  public GetOrganizationResponse getOrganization(GetOrganizationRequest request) throws ServerResponse {
+    ServerResponse sr = new ServerResponse();
+
+    Organization organization = oda.getOrganization(request.getOrganizationId());
+
+    if (organization == null) {
+      sr.addError("Organization was not found.");
+      throw sr;
     }
 
-    public GetOrganizationResponse getOrganization(GetOrganizationRequest request) throws ServerResponse {
-        ServerResponse sr = new ServerResponse();
+    // Generate the response object.
+    GetOrganizationResponse response = new GetOrganizationResponse(
+    organization.getId(),
+    organization.getName(),
+    organization.getLegalName(),
+    organization.getCuit()
+    );
 
-        Organization organization = oda.getOrganization(request.getOrganizationId());
+    return response;
+  }
 
-        if (organization == null) {
-            sr.addError("Organization was not found.");
-            throw sr;
-        }
+  public ArrayList<GetOrganizationResponse> getOrganizations() throws ServerResponse {
+    ServerResponse sr = new ServerResponse();
+    ArrayList<GetOrganizationResponse> response = new ArrayList<GetOrganizationResponse>();
 
-        // Generate the response object.
-        GetOrganizationResponse response = new GetOrganizationResponse(
-            organization.getId(),
-            organization.getName(),
-            organization.getLegalName(),
-            organization.getCuit()
-        );
+    // Fetch organization list.
+    ArrayList<Organization> organizations = oda.getOrganizations();
 
-        return response;
+    if (organizations == null || organizations.isEmpty()) {
+      sr.addError("Couldn't find organizations.");
+      throw sr;
     }
 
-    public ArrayList<GetOrganizationResponse> getOrganizations() throws ServerResponse {
-        ServerResponse sr = new ServerResponse();
-        ArrayList<GetOrganizationResponse> response = new ArrayList<GetOrganizationResponse>();
-
-        // Fetch organization list.
-        ArrayList<Organization> organizations = oda.getOrganizations();
-
-        if (organizations == null || organizations.isEmpty()) {
-            sr.addError("Couldn't find organizations.");
-            throw sr;
-        }
-
-        // Generate the response object.
-        for (Organization organization : organizations) {
-            response.add(new GetOrganizationResponse(
-            organization.getId(),
-            organization.getName(),
-            organization.getLegalName(),
-            organization.getCuit()
-            ));
-        }
-
-        return response;
+    // Generate the response object.
+    for (Organization organization : organizations) {
+      response.add(new GetOrganizationResponse(
+        organization.getId(),
+        organization.getName(),
+        organization.getLegalName(),
+        organization.getCuit()
+      ));
     }
+
+    return response;
+  }
 }
