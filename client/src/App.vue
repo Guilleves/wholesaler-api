08aa628566474a4254a83f614f8f5a3fa6215e8a
Juan Cruz Grasso <jgrasso@primerosystems.com>
2018-05-19 18:43:58 -0300

    router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="app">
-      <div class="container">
-          <div class="notification">
-              What on <strong>earth</strong> am I doing with my life?
-          </div>
-      </div>
+      <section>
+
+        <b-collapse :open="false">
+            <button class="button is-primary" slot="trigger">Click me!</button>
+            <div class="notification">
+                <div class="content">
+                    <h3>
+                        Subtitle
+                    </h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br/>
+                        Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. <br/>
+                        Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.
+                    </p>
+                </div>
+            </div>
+        </b-collapse>
+
+    </section>
   </div>
 </template>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Buefy from 'buefy';
+import VueRouter from 'vue-router';
 import 'buefy/lib/buefy.css';
 
-Vue.use(Buefy);
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+Vue.use(Buefy);
 
 new Vue({
   render: h => h(App)
