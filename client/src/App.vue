0b182b0d7e9d4a394bee83260c79dcd44516b0b1
Juan Cruz Grasso <jgrasso@primerosystems.com>
2018-05-19 18:11:54 -0300

    Refactoring and creating the client app

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <div class="container">
+          <div class="notification">
+              What on <strong>earth</strong> am I doing with my life?
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
